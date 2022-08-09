04120c2920d5bff0a0321db5bf834894acbcf311
Harikesh9690 <harikesmtr02018@gmail.com>
2022-08-09 21:46:48 +0530

    my query-param

--- src/routes/route.js
+++ src/routes/route.js
@@ -102,6 +102,60 @@ router.get("/films/:filmId", function(req, res){
        //if there is no match give an error response
        res.send("The film id doesn't match any movie")
 })
+// Note: The below questions are very commonly asked in nodejs interviews 
+
+// Q1.
+// -write an api which gives the missing number in an array of integers starting from 1….e.g [1,2,3,5,6,7] : 4 is missing
+// Your route code will look like this
+
+router.get("/sol1", function (req, res) {
+
+//logic : sum of numbers is n(n+1)/2..so get sum of all numbers in array. now take sum of numbers till last digit in the array
+
+    let arr1 = [1,2,3,5,6,7]
+    let missingNumber1
+             
+       ///LOGIC WILL GO HERE 
+       let sum1 =  ((arr1.length+1)*(arr1.length+2))/2
+       ///n*(n+1)/2              
+       let sumArray1 = 0
+       for(let i = 0; i < arr1.length; i++) {
+          sumArray1 = sumArray1 + arr1[i]
+       }
+
+       missingNumber1 = sum1 - sumArray1
+       
+       res.send(  { data: missingNumber1  }  );
+});
+
+
+// Q2. 
+// -write an api which gives the missing number in an array of integers starting from anywhere….e.g [33, 34, 35, 37, 38]: 36 is missing
+// Your route code will look like this
+
+router.get("/sol2", function (req, res) {
+
+//logic : sum of n consecutive numbers is [ n * (first + last) / 2  ]..so get sum of all numbers in array. now take sum of n consecutive numbers.. n would be length+1 as 1 number is missing
+        
+let arr2 = [33, 34, 35, 37, 38]
+let missingNumber2
+
+///LOGIC WILL GO HERE 
+let lam = arr2.length
+let marr = 0
+    for (const i in  arr2) {
+       marr = marr + arr2[i]
+    } 
+
+let fnum = arr2[0]
+let lnum = arr2.pop()
+let carr = (lam +1) *(fnum + lnum)/2
+
+missingNumber2  = carr - marr
+res.send(  { data: missingNumber2  }  );
+
+});
+
 
 module.exports = router;
 // adding this comment for no reason
