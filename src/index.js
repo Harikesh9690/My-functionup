f5779c4313e0975a5f4a9c291bb0c2b57a7e0c56
Harikesh9690 <harikesmtr02018@gmail.com>
2022-08-12 19:57:52 +0530

    hello

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect("mongodb+srv://functionup-cohort:3bMssIUqmBmIqb3e@cluster0.3ccgp0v.mongodb.net/PK-100?retryWrites=true&w=majority"
+mongoose.connect("mongodb+srv://Harikesh9690:1MUWVL27E2RUlahK@cluster0.wxlsnqz.mongodb.net/harikesh-DB?retryWrites=true&w=majority"
 , {
    useNewUrlParser: true 
 }
