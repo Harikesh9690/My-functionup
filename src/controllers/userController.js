e4ff0a5e5c78a5c1fe8d3a6041ff79b6916c5e7e
Harikesh9690 <harikesmtr02018@gmail.com>
2022-09-01 14:36:55 +0530

    try-catch

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,46 +1,89 @@
-const UserModel= require("../models/userModel")
+const userModel = require("../models/userModel")
+const jwt = require('jsonwebtoken')
 
 
+const createUser = async function (req, res) {
+  try {
+    let data = req.body;
+    let savedData = await userModel.create(data);
+    res.status(201).send({ msg: savedData });
+  } catch (error) {
+    res.status(500).send(error.message)
+  }
+};
 
+const loginUser = async function (req, res) {
+  try {
+    let userName = req.body.emailId;
+    let password = req.body.password;
 
-const basicCode= async function(req, res) {
-    let tokenDataInHeaders= req.headers.token
-    console.log(tokenDataInHeaders)
+    let user = await userModel.findOne({ emailId: userName, password: password });
+    if (!user)
+      return res.status(401).send({
+        status: false,
+        msg: "username or the password is not correct",
+      });
 
-    console.log( "HEADER DATA ABOVE")
-    console.log( "hey man, congrats you have reached the Handler")
-    res.send({ msg: "This is coming from controller (handler)"})
+    let token = jwt.sign(
+      {
+        userId: user._id.toString(),
+        batch: "thorium",
+        organisation: "FunctionUp",
+      },
+      "functionup-plutonium-very-very-secret-key"
+    );
+    res.setHeader("x-auth-token", token);
+    res.status(200).send({ status: true, token: token });
+  } catch (error) {
+    res.status(500).send(error.message)
+  }
+};
+
+const getUserData = async function (req, res) {
+  try {
+    let userId = req.params.userId;
+    if (userId) {
+      let userDetails = await userModel.findById(userId);
+      res.send({ status: true, data: userDetails });
+    } else {
+      return res.status(401).send({ status: false, msg: "No such user exists" });
+    }
+  } catch (error) {
+    res.status(500).send(error.message)
+  }
+};
+
+const updateUser = async function (req, res) {
+  try {
+    let userId = req.params.userId;
+    let user = await userModel.findById(userId);
+    if (!user) {
+      return res.send("No such user exists");
     }
 
+    let userData = req.body;
+    let updatedUser = await userModel.findOneAndUpdate({ _id: userId }, userData);
+    res.send({ status: updatedUser, data: updatedUser });
+  } catch (error) {
+    res.status(500).send(error.message)
+  }
+};
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const createUser= async function (req, res) {
-    let data= req.body
-    let savedData= await UserModel.create(data)
-    res.send({msg: savedData})
+const deleteuser = async function (req, res) {
+  try {
+    let userId = req.params.userId
+    if (req.pass.userId == userId) {
+      let userDetails = await userModel.findByIdAndUpdate({ _id: userId }, { $set: { isdeleted: true } })
+      res.status(200).send({ status: true, msg: "deleted successfully" })
+    } else {
+      res.status(403).send({ status: false, msg: "you are not authorised for this opretion" })
+    }
+  } catch (error) {
+    res.status(500).send(error.message)
+  }
 }
-
-const getUsersData= async function (req, res) {
-    let allUsers= await UserModel.find()
-    res.send({msg: allUsers})
-}
-
-module.exports.createUser= createUser
-module.exports.getUsersData= getUsersData
-module.exports.basicCode= basicCode
+module.exports.createUser = createUser;
+module.exports.getUserData = getUserData;
+module.exports.updateUser = updateUser;
+module.exports.loginUser = loginUser;
+module.exports.deleteuser = deleteuser;
--- src/middlewares/commonMiddlewares.js
+++ src/middlewares/commonMiddlewares.js
@@ -1,26 +1,16 @@
+const jwt = require('jsonwebtoken')
 
-const mid1= function ( req, res, next) {
-    req.falana= "hi there. i am adding something new to the req object"
-    console.log("Hi I am a middleware named Mid1")
-    next()
+let auth2 = function(req, res, next){
+let token = req.headers['x-auth-tokan']
+  if (!token) {
+    res.send({ status: false, msg: "token must be present" })
+  }
+  let decodedToken = jwt.verify(token, "functionup-plutonium-very-very-secret-key")
+  if (!decodedToken) {
+    res.send({ status: false, msg: "tokan is invalid" })
+  }  
+  req.pass = decodedToken
+  next()
 }
 
-const mid2= function ( req, res, next) {
-    console.log("Hi I am a middleware named Mid2")
-    next()
-}
-
-const mid3= function ( req, res, next) {
-    console.log("Hi I am a middleware named Mid3")
-    next()
-}
-
-const mid4= function ( req, res, next) {
-    console.log("Hi I am a middleware named Mid4")
-    next()
-}
-
-module.exports.mid1= mid1
-module.exports.mid2= mid2
-module.exports.mid3= mid3
-module.exports.mid4= mid4
+module.exports.auth2 = auth2;
